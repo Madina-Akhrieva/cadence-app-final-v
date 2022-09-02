ce4de5e614d97cf9df212879f20e41ddeef4d947
Madina-Akhrieva <akhrieva03@gmail.com>
2022-09-02 10:44:27 +0300

    refactor commit: put files under git control

---
+++ weather-cadence-app/src/test/java/com/example/weathercadenceapp/WeatherCadenceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weathercadenceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCadenceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
