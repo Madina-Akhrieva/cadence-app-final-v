bc8d6ca2dcc0feeac62dd68e1642c9adf455bb47
Madina-Akhrieva <akhrieva03@gmail.com>
2022-09-02 10:49:37 +0300

    refactor commit: add main workflow

---
+++ weather-cadence-app/src/main/java/com/example/weathercadenceapp/service/MainWorkflowWorker.java
@@ -0,0 +1,15 @@
+package com.example.weathercadenceapp.service;
+
+import com.uber.cadence.worker.Worker;
+import com.uber.cadence.worker.WorkerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainWorkflowWorker {
+
+    public Worker startMainWorkflowWorker(WorkerFactory factory) {
+        Worker worker = CadenceServiceUtil.getWorker(factory, CadenceServiceUtil.TASK_LIST);
+        worker.registerWorkflowImplementationTypes(TemperatureWorkflowImpl.class);
+        return worker;
+    }
+}
